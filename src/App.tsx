9693cf9eafc011ed86330028393f0e30adebcb91
Joey Vano <joseph@vano.io>
2021-01-21 09:11:23 -0500

    initial commit with card basic components

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { AppContainer } from './styled';
+import { Card } from './Card';
+import { Column } from './Column';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppContainer>
+      <Column text="To do">
+        <Card text="Generate app scaffold"></Card>
+      </Column>
+      <Column text="In Progress">
+        <Card text="Learn TypeScript"></Card>
+      </Column>
+      <Column text="Done">
+        <Card text="Begin to use static typing"></Card>
+      </Column>
+    </AppContainer>
   );
 }
 
---
+++ src/Card.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { CardContainer } from './styled';
+
+interface CardProps {
+  text: string;
+}
+
+export const Card = ({text}: CardProps) => {
+  return (
+    <CardContainer>
+      {text}
+    </CardContainer>
+  );
+}
+
+
